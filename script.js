758432f43c3123b61e43f3b45310fe44c6ef9259
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-07-07 20:46:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
-const lookup = {
-  A: "N",
-  B: "O",
-  C: "P",
-  D: "Q",
-  E: "R",
-  F: "S",
-  G: "T",
-  H: "U",
-  I: "V",
-  J: "W",
-  K: "X",
-  L: "Y",
-  M: "Z",
-  N: "A",
-  O: "B",
-  P: "C",
-  Q: "D",
-  R: "E",
-  S: "F",
-  T: "G",
-  U: "H",
-  V: "I",
-  W: "J",
-  X: "K",
-  Y: "L",
-  Z: "M",
-  "?": "?",
-  ",": ",",
-};
-
-function rot13(encodedString) {
-  let decodedString = "";
-
-  for (let i = 0; i < encodedString.length; i++) {
-    const character = encodedString[i];
-    const decodedCharacter = lookup[character];
-
-    if (decodedCharacter) {
-      decodedString += decodedCharacter;
+function rot13(str) {
+  var decoded = "";
+  for (var i = 0; i < str.length; i++) {
+    var charCode = str.charCodeAt(i);
+    if (charCode >= 65 && charCode <= 90) {
+      // Uppercase letters (A-Z)
+      decoded += String.fromCharCode(((charCode - 65 + 13) % 26) + 65);
+    } else if (charCode >= 97 && charCode <= 122) {
+      // Lowercase letters (a-z)
+      decoded += String.fromCharCode(((charCode - 97 + 13) % 26) + 97);
     } else {
-      decodedString += character;
+      // Non-alphabetic characters
+      decoded += str.charAt(i);
     }
   }
-
-  return decodedString;
+  return decoded;
 }
 
+function decode() {
+  var input = document.getElementById("input").value;
+  var output = rot13(input);
+  document.getElementById("output").value = output;
+}
